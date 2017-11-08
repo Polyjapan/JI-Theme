df6c36ac4f1a87e5473806b10a658eadde35c060
zyuiop <zyuiop@zyuiop.net>
2017-11-08 15:14:29 +0100

    Add a footer navigation

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,44 +1,8 @@
 <?php
 /* Creation du menu dynamiquement */
 $menu_name = 'header-menu';
-if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) 
-{
-  $menu_array = array();
-  $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
-
-  $menu_items = wp_get_nav_menu_items($menu->term_id);
+if (!empty($menu_array = get_menu($menu_name))) {
   $menu_list = '';
-              /**
-                Organise le menu dans un array comme cela :
-                Array ( [145] => Array ( [name] => Éditions Précédentes
-                             [url] => http://127.0.0.1/wordpress/?page_id=42 )
-                    [141] => Array ( [name] => horaire 
-                             [url] => http://127.0.0.1/wordpress/?page_id=131 ) 
-                    [143] => Array ( [name] => Comité 
-                             [url] => http://127.0.0.1/wordpress/?page_id=44 
-                             [0] => Comité 2010 
-                             [1] => Comité 2009 ) 
-                    )
-               */
-  foreach ( (array) $menu_items as $key => $menu_item ) 
-  {
-    $title = $menu_item->title;
-    $url = $menu_item->url;
-    $id = $menu_item->ID;
-    $id_parent = $menu_item->menu_item_parent;
-    $post_par = $menu_item->post_parent;
-    if($id_parent == 0 ) // si c'est un parent
-    {
-      $menu_array[$id] = array();
-      $menu_array[$id]['name'] = $title;
-      $menu_array[$id]['url'] = $url;
-    }
-    else // c'est un enfant
-    {
-      $item = '<a href="'.$url.'">'.$title.'</a>';
-      array_push($menu_array[$id_parent],$item);
-    }
-  }
 
   /* Création du code html selon l'array */
   foreach((array) $menu_array as $element )
